7e490fa82ba932c7dec6f9289ed67da8cb38c077
choch-o <hscho1226@gmail.com>
2016-12-26 22:56:45 +0900

    Implemented tab2 gallery

--- app/src/main/java/com/example/q/project1/Tab2Fragment.java
+++ app/src/main/java/com/example/q/project1/Tab2Fragment.java
@@ -1,18 +1,34 @@
 package com.example.q.project1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.GridLayoutAnimationController;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 // Instances of this class are fragments representing a single
 // object in our collection.
 public class Tab2Fragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.tab2, container, false);
+        View rootView = inflater.inflate(R.layout.tab2, container, false);
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(getActivity()));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                /* Intent i = new Intent(getActivity(), );
+                i.putExtra("position", position);
+                getActivity().startActivity(i); */
+            }
+        });
+        return rootView;
     }
 }
